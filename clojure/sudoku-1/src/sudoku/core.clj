048763ce9f847f05db5bb7310539a4f7521ebd04
Fellipe Brito <fellipe@devlandia.net>
2017-01-18 16:35:21 -0800

    Adds one basic raw example of grid

--- clojure/sudoku-1/src/sudoku/core.clj
+++ clojure/sudoku-1/src/sudoku/core.clj
@@ -8,15 +8,30 @@
   (and (false? (has-duplicates? group))
        (= (reduce + (map #(Character/digit % 10) group)) 45)))
 
+(defn columns [rows]
+  [(apply str (map #(nth % 0) rows))
+   (apply str (map #(nth % 1) rows))
+   (apply str (map #(nth % 2) rows))
+   (apply str (map #(nth % 3) rows))
+   (apply str (map #(nth % 4) rows))
+   (apply str (map #(nth % 5) rows))
+   (apply str (map #(nth % 6) rows))
+   (apply str (map #(nth % 7) rows))
+   (apply str (map #(nth % 8) rows))])
+
 (defn rip-the-puzzle [puzzle]
-  {:rows (re-seq #".{1,9}" puzzle)})
+  (let [rows (re-seq #".{1,9}" puzzle)]
+    {:rows (into [] rows)
+     :cols (columns rows)}))
 
 (defn -main
   [& args]
   (let [size    (read-string (read-line))
         puzzles (re-seq #".{1,81}" (str/replace (str/replace (slurp *in*) "\n" "") " " ""))]
-    (if (every? true?
-                (map valid-group? (:rows (rip-the-puzzle (first puzzles)))))
+    (if (and (every? true?
+                     (map valid-group? (:rows (rip-the-puzzle (first puzzles)))))
+             (every? true?
+                     (map valid-group? (:cols (rip-the-puzzle (first puzzles))))))
       (println "Valid")
       (println "Invalid")
       )))
--- clojure/sudoku-1/test/sudoku/core_test.clj
+++ clojure/sudoku-1/test/sudoku/core_test.clj
@@ -4,17 +4,27 @@
             [sudoku.core :refer :all]))
 
 (def valid-puzzle
-  [(str/replace "5 3 4 6 7 8 9 1 2 6 7 2 1 9 5 3 4 8 1 9 8 3 4 2 5 6 7 8 5 9 7 6 1 4 2 3 4 2 6 8 5 3 7 9 1 7 1 3 9 2 4 8 5 6 9 6 1 5 3 7 2 8 4 2 8 7 4 1 9 6 3 5 3 4 5 2 8 6 1 7 9" " " "")])
+  (str/replace "5 3 4 6 7 8 9 1 2 6 7 2 1 9 5 3 4 8 1 9 8 3 4 2 5 6 7 8 5 9 7 6 1 4 2 3 4 2 6 8 5 3 7 9 1 7 1 3 9 2 4 8 5 6 9 6 1 5 3 7 2 8 4 2 8 7 4 1 9 6 3 5 3 4 5 2 8 6 1 7 9" " " ""))
 
 (def invalid-puzzle
-  [(str/replace "5 5 4 6 7 8 9 1 2 6 7 2 1 9 5 3 4 8 1 9 8 3 4 2 5 6 7 8 5 9 7 6 1 4 2 3 4 2 6 8 5 3 7 9 1 7 1 3 9 2 4 8 5 6 9 6 1 5 3 7 2 8 4 2 8 7 4 1 9 6 3 5 3 4 5 2 8 6 1 7 9" " " "")])
+  (str/replace "5 5 4 6 7 8 9 1 2 6 7 2 1 9 5 3 4 8 1 9 8 3 4 2 5 6 7 8 5 9 7 6 1 4 2 3 4 2 6 8 5 3 7 9 1 7 1 3 9 2 4 8 5 6 9 6 1 5 3 7 2 8 4 2 8 7 4 1 9 6 3 5 3 4 5 2 8 6 1 7 9" " " ""))
 
 (deftest rip-the-puzzle-test
   (testing "a puzzle has 9 rows"
-    (is (= (count (:rows (rip-the-puzzle valid-puzzle))) 9))))
+    (is (= (count (:rows (rip-the-puzzle valid-puzzle))) 9)))
+  (testing "a puzzle has 9 cols"
+    (is (= (count (:cols (rip-the-puzzle valid-puzzle))) 9))))
 
 (deftest valid-group-test
   (testing "a valid group"
     (is (true? (valid-group? (apply str (shuffle (range 1 10)))))))
   (testing "an invalid group"
     (is (false? (valid-group? "112345678")))))
+
+(defn grid [cols rows]
+  [(apply str [(nth (nth rows 0) 0) (nth (nth rows 0) 1) (nth (nth rows 0) 2)
+               (nth (nth rows 1) 0) (nth (nth rows 1) 1) (nth (nth rows 1) 2)
+               (nth (nth rows 2) 0) (nth (nth rows 2) 1) (nth (nth rows 2) 2)
+               ])])
+
+(grid (:cols (rip-the-puzzle valid-puzzle)) (:rows (rip-the-puzzle valid-puzzle)))
